304dc479d22ce575ce471ce0a1c3ff0e977cc1ec
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-06-22 11:27:00 -0700

    Update eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -15,8 +15,24 @@ def eating_cookies(n):
         return 4
     return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
+
+# cache is a dict and keys are n - the value is the answer
+    def eating_cookies(n, cache):
+    if n < 0:
+        return 1
+    elif n == 0:
+        return 1
+    elif cache[n] > 0:
+        return cache[n]
+    else:
+        # if cache doesnt contain answer well use recursion
+        # and save results for the future:
+        cache[n] = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    return cache[n]
+
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
+    cache = {i: 0 for i in range(num_cookies+1)}
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
